354a6655a70d08c036788db0893170a4f8e1d486
Adrien <Adnn@users.noreply.github.com>
2021-10-20 11:45:15 +0200

    ISSUE: The clipping code still generate invalide positions (near plane).

--- src/apps/ch8/graphics_pipeline/main.cpp
+++ src/apps/ch8/graphics_pipeline/main.cpp
@@ -184,8 +184,9 @@ void renderPerspectiveCube(filesystem::path aImageFilePath, math::Size<2, int> a
 
     focg::TransformAndLighting program;
 
-    const double nearPlaneZ = -20;
-    const double farPlaneZ = -149;
+    // ISSUE: Clipping from near plane generate incorrect vertices positions.
+    const double nearPlaneZ = -60;
+    const double farPlaneZ = -150;
     const double depth = nearPlaneZ - farPlaneZ;
 
     const double cubeSize = 100.;
